8f6fc4ad87d2fb59f65a5300e7b93da776aaabc3
Guilherme Batoni <batoni.15@hotmail.com>
2024-08-12 16:24:40 -0300

    add readme

--- aula11/ex001.js
+++
@@ -1,7 +0,0 @@
-var vel = 12
-console.log(`A velocidade do seu carro é de ${vel}`)
-if (vel > 60) {
-    console.log('Você ultrapassou a velocidade permitida, você foi MULTADO!')
-
-}
-console.log('Dirija sempre com cinto de segurança.')
--- aula11/ex002.js
+++
@@ -1,9 +0,0 @@
-var pais = 'EUA'
-console.log(`Seu país é ${pais}.`)
-if(pais != 'Brasil') {
-    console.log('Você é Estrangeiro')
-
-}
-else { 
-    console.log('Você é Brasileiro.')
-}
